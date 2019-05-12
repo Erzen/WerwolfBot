27b862e15dd32aaaf8c7bd976e8024e71cd3b3c2
Sven <doccox@hotmail.de>
2019-05-12 20:11:10 +0200

    logging for file saving and reading

--- werwolf/utils/file_util.py
+++ werwolf/utils/file_util.py
@@ -1,16 +1,22 @@
+import logging
 import os.path as path
+
 try:
     import cPickle as pickle
 except ModuleNotFoundError:
     import pickle
+
+logger = logging.getLogger(__name__)
+
 
 def save_object(obj, filename):
     file_path = "./pickleFiles/{}".format(filename)
     try:
         with open(file_path, 'wb') as output:  # Overwrites any existing file.
             pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
-    except IOError:
+    except IOError as e:
         print("An Error occured during the writing of the File '{}'".format(file_path))
+        logger.exception(e)
 
 def load_object(filename):
     file_path = "./pickleFiles/{}".format(filename)
@@ -19,6 +25,7 @@ def load_object(filename):
             with open(file_path, 'rb') as input:  # Overwrites any existing file.
                 object = pickle.load(input)
             return object
-        except IOError:
+        except IOError as e:
             print("An Error occured during the reading of the File '{}'".format(file_path))
+            logger.exception(e)
     return None
