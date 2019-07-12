02b6e3a7e58eaf6c9dd4aeaf56a7ae6f275c824e
Linda <48601642+Linn214@users.noreply.github.com>
2019-07-12 08:04:58 +0200

    Texte angepasst

--- werwolf/utils/text.py
+++ werwolf/utils/text.py
@@ -1,7 +1,7 @@
 def get_help_text():
     return "__**Commands für everyone**__\n\
 ```\n\
-/createGame Emoji [optionale Message für den invite]\n\
+/createGame [Emoji] [Channel] [Text der Nachricht]\n\
 - erstellt Spiel\n\
 ```\n\
 __**Commands für Spielleitung**__\n\
@@ -10,31 +10,31 @@ __**Commands für Spielleitung**__\n\
 - fügt Spieler dem Spiel zu\n\
 ``````\n\
 /kill @Spieler @Spieler\n\
-- nimmt demjenigen Lebendig und setzt ihn auf tot.\n\
+- nimmt dem Spieler Lebendig und setzt ihn auf Tot\n\
 ``````\n\
 /revive @Spieler @Spieler\n\
-- nimmt demjenigen Tot und setzt ihn auf Lebendig.\n\
+- nimmt dem Spieler Tot und setzt ihn auf Lebendig\n\
 ``````\n\
 /listPlayers\n\
 - listet alle Spieler auf\n\
 ``````\n\
 /close\n\
-- beendet das Spiel und entfernt @Lebendig und @Tot\n\
+- beendet das Spiel und entfernt Lebendig und Tot\n\
 ``````\n\
 /kick @Spieler1 @Spieler2\n\
 - entfernt Spieler vom Spiel\n\
 ``````\n\
+/pool 3 Werwolf Nutte Hexe Jäger 3 Dorfbewohner\n\
+- erstellt einen Rollenpool der in der Phase 'gaming' verteilt wird\n\
+``````\n\
 /next\n\
-- Wechselt von Spielphase 'inviting' -> 'checking_roles' -> 'gaming' und verteilt Rollen bei 'gaming'\n\
+- wechselt von Spielphase 'inviting' -> 'checking_roles' -> 'gaming' und verteilt Rollen bei 'gaming'\n\
+``````\n\
+/shuffle\n\
+- während der Spielphase 'checking_roles' kann die Rollenverteilung nochmal durchmischt werden\n\
 ``````\n\
 /back\n\
 - wechselt zurück zur Phase 'inviting'\n\
-``````\n\
-/pool 3 db 3 ww hexe amor jäger\n\
-- erstellt einen Rollenpool der bei /next verteilt wird\n\
-``````\n\
-/shuffle\n\
-- Während der Spielphase 'checking_roles' kann die Rollenverteilung nochmal durchmischt werden\n\
 ``````\n\
 /leader @Spieler\n\
 - gibt die Spielleitung an @Spieler ab (noch buggy)\n\
@@ -44,7 +44,7 @@ def get_greeting_text(member):
     return "Hey {}, :heart:-lich willkommen in unserer Community! Schön, dass du her gefunden hast. <:WolfHeart:561592501257895936>\n\
 Der Server Werwolf steht grundsätzlich für das Gesellschaftsspiel \"Werwölfe von Düsterwald\". \
 Wir spielen fast täglich auf dem Server. Neben Werwolf bieten wir noch andere Spiele, wie Secret Hitler\
-, Werwolf Vollmondnacht/Morgengrauen, skribbl.io usw. an. Diese sowie alles Weitere findest du links unter \
- der Kategorie \"Wichtiges\". Um Berechtigungen für alle Channel auf dem Server zu erhalten, reagiere im #info-Channel \
-mit dem :white_check_mark:. Fühl dich wie zu Hause - bei Unklarheiten einfach fragen. Wir freuen uns, bald\
- mit dir zu spielen! <:HappyTessa:490482340116561921>".format(member.nick if member.nick else member.name)
+, Werwolf Vollmondnacht/Morgengrauen, skribbl.io usw. an. Diese sowie alles Weitere findest du \
+auf unserem Discord. Um Berechtigungen für alle Channel auf dem Server zu erhalten, reagiere im Channel <#463009130777411584> \
+mit dem :white_check_mark:. Fühl dich wie zu Hause - bei Unklarheiten einfach fragen. Wir freuen uns, bald \
+mit dir zu spielen! <:HappyTessa:490482340116561921>".format(member.nick if member.nick else member.name)
